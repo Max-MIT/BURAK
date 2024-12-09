8bb8a1a302aead465525409c60e598eeafbf3c33
Max-MIT <maksudbek198501@gmail.com>
2024-12-09 22:13:29 +0900

    TASK-H

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+function getPositive(arr: number[]): string {
+    // Faqat musbat sonlarni tanlab, string holatida qaytaramiz
+    const positiveNumbers = arr.filter(num => num > 0);
+    return positiveNumbers.join('');
+}
+
+// Misol:
+console.log(getPositive([1, -4, 2])); // Natija: "12"
+
