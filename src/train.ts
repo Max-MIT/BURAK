25895ce04c65924ad441ea69353589516369ef8d
Max-MIT <maksudbek198501@gmail.com>
2024-12-11 23:04:06 +0900

    H2-TASK

--- src/train.ts
+++ src/train.ts
@@ -1,11 +1,29 @@
-// console.log("Hello World!");
+// H2-TASK: 
 
-function getPositive(arr: number[]): string {
-    // Faqat musbat sonlarni tanlab, string holatida qaytaramiz
-    const positiveNumbers = arr.filter(num => num > 0);
-    return positiveNumbers.join('');
-}
+// Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+// MASALAN: getDigits("m14i1t") return qiladi "141"   ts
 
-// Misol:
-console.log(getPositive([1, -4, 2])); // Natija: "12"
+
+function getDigits(input: string): string {
+    return input.replace(/\D/g, ""); // \D bu "digit emas" degani, uni bo'sh joy bilan almashtiramiz
+  }
+  
+  // Sinov
+  console.log(getDigits("m14i1t")); // "141"
+  console.log(getDigits("abc123xyz456")); // "123456"
+  console.log(getDigits("no-digits")); // ""
+  
+
+
+
+// // console.log("Hello World!");
+
+// function getPositive(arr: number[]): string {
+//     // Faqat musbat sonlarni tanlab, string holatida qaytaramiz
+//     const positiveNumbers = arr.filter(num => num > 0);
+//     return positiveNumbers.join('');
+// }
+
+// // Misol:
+// console.log(getPositive([1, -4, 2])); // Natija: "12"
 
