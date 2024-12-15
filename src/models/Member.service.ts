9275819b8ba442bd1d3e82fcdcbd3416f664ce95
Max-MIT <maksudbek198501@gmail.com>
2024-12-15 21:21:20 +0900

    feat: develop restaurant login process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,6 +1,6 @@
 import MemberModel from "../schema/Member.model";
-import { Member, MemberInput } from "../libs/types/member";
-import Errors, {HttpCode, Message} from "../libs/Errors";
+import { LoginInput, Member, MemberInput } from "../libs/types/member";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
 
 class MemberService {
@@ -12,7 +12,7 @@ class MemberService {
 
 public async processSignup(input: MemberInput): Promise<Member> {
     const exist = await this.memberModel
-      .findOne({memberType: MemberType.RESTAURANT})
+      .findOne({ memberType: MemberType.RESTAURANT })
       .exec();
     if(exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
     
@@ -23,8 +23,24 @@ public async processSignup(input: MemberInput): Promise<Member> {
     } catch (err) {
         throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
     }
+}
 
-  }
+public async processLogin(input: LoginInput): Promise<Member> {
+const member = await this.memberModel
+.findOne(
+    { memberNick: input.memberNick }, 
+    { memberNick: 1, memberPassword: 1}
+)
+.exec();
+if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK); 
+
+const isMatch = input.memberPassword === member.memberPassword;
+if (!isMatch) {
+  throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+}
+
+return await this.memberModel.findById(member._id).exec();
+ }
 }
 
 export default MemberService;
