a66867907db1e1fda86279c3e7efcf3a82a13a20
Max-MIT <maksudbek198501@gmail.com>
2024-12-23 21:27:30 +0900

    fix: create sessions on login and signup process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { LoginInput, MemberInput } from "../libs/types/member";
+import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
 const memberService = new MemberService();
@@ -11,7 +11,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
     try {
       console.log("goHome");
       res.render("home");
-    } catch(err) {
+    } catch (err) {
         console.log("Error, goHome:", err);
     }
 };
@@ -20,7 +20,7 @@ restaurantController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("getSignup");
     res.render("signup");
-  } catch(err) {
+  } catch (err) {
       console.log("Error, getSignup:", err);
   }
 };
@@ -29,36 +29,46 @@ restaurantController.getLogin = (req: Request, res: Response) => {
     try {
       console.log("getLogin");
       res.render("login");
-    } catch(err) {
+    } catch (err) {
         console.log("Error, getLogin:", err);
     }
 };
 
-restaurantController.processSignup = async (req: Request, res: Response) => {
+restaurantController.processSignup = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
-    console.log("processSignup");
+    console.log("processSignup:");
     
     const newMember: MemberInput = req.body;
     newMember.memberType = MemberType.RESTAURANT;
     const result = await memberService.processSignup(newMember);
-      // TOOD: SESSIONS AUTHENTICATION
 
-    res.send(result);
-      } catch (err) {
-      console.log("Error, processSignup", err);
+      req.session.member = result;
+      req.session.save(function () {
+        res.send(result); 
+      });
+    } catch (err) {
+      console.log("Error, processSignup:", err);
       res.send(err);
   }
 };
 
-restaurantController.processLogin = async (req: Request, res: Response) => {
+restaurantController.processLogin = async (
+   req: AdminRequest,
+   res: Response
+) => {
   try {
     console.log("processLogin");
 
     const input: LoginInput = req.body;
     const result = await memberService.processLogin(input);
-// TOOD: SESSIONS AUTHENTICATION
 
-    res.send(result);
+req.session.member = result;
+      req.session.save(function () {
+        res.send(result); 
+      });
       } catch(err) {
       console.log("Error, processLogin:", err);
       res.send(err);
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -54,7 +54,7 @@ public async processSignup(input: MemberInput): Promise<Member> {
     const exist = await this.memberModel
       .findOne({ memberType: MemberType.RESTAURANT })
       .exec();
-    if(exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
     
 const salt = await bcrypt.genSalt();
 input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
