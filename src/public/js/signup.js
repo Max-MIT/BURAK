5f4abf00684bfcf6323cc8508129c87b71156f5d
Max-MIT <maksudbek198501@gmail.com>
2025-01-09 20:16:03 +0900

    fix: admin project refactoring

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -6,11 +6,11 @@ $(function () {
 
     fileTarget.on("change", function () {
     if (window.FileReader) {
-        const uploadFile = $(this)[0].files[0];
-        const fileType = uploadFile["type"];
-        const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+        const uploadFile = $(this)[0].files[0],
+        fileType = uploadFile["type"],
+        validImageType = ["image/jpg", "image/jpeg", "image/png"];
         if( !validImageType.includes(fileType)) {
-            alert("Please insert only jpeg, jpg, and png!")
+            alert("Please insert only jpeg, jpg, and png!");
     } else {
         if(uploadFile) {
             console.log(URL.createObjectURL(uploadFile));
@@ -27,9 +27,9 @@ $(function () {
 
 function validateSignupForm() {
  const memberNick = $(".member-nick").val();
- const memberPhone = $(".member-phone").val();
- const memberPassword = $(".member-password").val();
- const confirmPassword = $(".confirm-password").val();  
+ memberPhone = $(".member-phone").val();
+ (memberPassword = $(".member-password").val());
+ (confirmPassword = $(".confirm-password").val());  
 
  if (
     memberNick === "" ||
