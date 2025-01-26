43987f1331dc85f77988337435f0c610c7c4c45f
Max-MIT <maksudbek198501@gmail.com>
2025-01-26 17:17:10 +0900

    fix: modify getMemberDetail business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,7 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { ExtendedRequest, LoginInput, Member, MemberInput } from "../libs/types/member";
+import { 
+  ExtendedRequest, 
+  LoginInput, 
+  Member, 
+  MemberInput 
+} from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
 import { AUTH_TIMER } from "../libs/config";
@@ -54,10 +59,26 @@ const memberController: T = {};
   memberController.logout = (req: ExtendedRequest, res: Response) => {
     try {
       console.log("logout");
-      res.cookie("accwssToken", null, { maxAge: 0, httpOnly: true });
+      res.cookie("accessToken", null, { maxAge: 0, httpOnly: true });
       res.status(HttpCode.OK).json({ logout: true });
     } catch(err) {
       console.log("Error, logout:", err);
+      if(err instanceof Errors) res.status(err.code).json(err);
+      else res.status(Errors.standard.code).json(Errors.standard);
+    }
+  }
+
+  memberController.getMemberDetail = async (
+    req: ExtendedRequest, 
+    res: Response
+  ) => {
+    try {
+      console.log("getMemberDetail");
+      const result = await memberService.getMemberDetail(req.member);
+
+      res.status(HttpCode.OK).json(result);
+    } catch(err) {
+      console.log("Error, getMemberDetail:", err);
       if(err instanceof Errors) res.status(err.code).json(err);
       else res.status(Errors.standard.code).json(Errors.standard);
     }
