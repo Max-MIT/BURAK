8cc367129b7c2641f54034fdc8832672c1675613
Max-MIT <maksudbek198501@gmail.com>
2025-01-27 17:51:49 +0900

    feat: creat getRestaurant rest Api

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -16,6 +16,20 @@ const memberService = new MemberService();
 const authService = new AuthService();
 
 const memberController: T = {};
+
+memberController.getRestaurant = async (req: Request, res: Response) => {
+  try {
+    console.log("getRestaurant"); 
+    const result = await memberService.getRestaurant();
+
+    res.status(HttpCode.OK).json(result);
+      } catch (err) {
+      console.log("Error, getRestaurant", err);
+      if(err instanceof Errors) res.status(err.code).json(err);
+      else res.status(Errors.standard.code).json(Errors.standard);
+  }
+}
+
   memberController.signup = async (req: Request, res: Response) => {
     try {
       console.log("signup"); 
@@ -103,7 +117,7 @@ const memberController: T = {};
     try {
       console.log("getTopUsers");
       const result = await memberService.getTopUsers();
-      
+
       res.status(HttpCode.OK).json(result);
       } catch(err) {
       console.log("Error, getTopUsers:", err);
