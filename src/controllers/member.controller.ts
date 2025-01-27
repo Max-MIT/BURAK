c7fe8461ddc64ce114f2654883bcbdc54bdff98d
Max-MIT <maksudbek198501@gmail.com>
2025-01-27 16:54:37 +0900

    feat: develop getTopUsers rest Api

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,7 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { ExtendedRequest, LoginInput, Member, MemberInput, MemberUpdateInput } from "../libs/types/member";
+import { 
+  ExtendedRequest, 
+  LoginInput, 
+  Member, 
+  MemberInput, 
+  MemberUpdateInput 
+} from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
 import { AUTH_TIMER } from "../libs/config";
@@ -92,6 +98,19 @@ const memberController: T = {};
       else res.status(Errors.standard.code).json(Errors.standard);
     }
   };
+
+  memberController.getTopUsers = async (req: Request, res: Response) => {
+    try {
+      console.log("getTopUsers");
+      const result = await memberService.getTopUsers();
+      
+      res.status(HttpCode.OK).json(result);
+      } catch(err) {
+      console.log("Error, getTopUsers:", err);
+      if(err instanceof Errors) res.status(err.code).json(err);
+      else res.status(Errors.standard.code).json(Errors.standard);
+    }
+  }
   
 
   memberController.verifyAuth = async (
