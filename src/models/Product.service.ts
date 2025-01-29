205691ba5bed0a4dbed52f43da3c3fba71ad964f
Max-MIT <maksudbek198501@gmail.com>
2025-01-29 16:38:15 +0900

    fix: modify getProduct business logic

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -10,12 +10,17 @@ import {
 import ProductModel from "../schema/Product.model";
 import { ProductStatus } from "../libs/enums/product.enum";
 import { ObjectId } from "mongoose";
+import ViewService from "./View.service";
+import { ViewInput } from "../libs/types/view";
+import { ViewGroup } from "../libs/enums/view.enum";
 
 class ProductServise {
     private readonly productModel;
+    public viewService;
 
     constructor() {
         this.productModel = ProductModel;
+        this.viewService = new ViewService();
     }
      
     /** SPA */
@@ -57,10 +62,34 @@ class ProductServise {
       .findOne({
         _id: productId, 
         productStatus: ProductStatus.PROCESS,
-      }).exec();
+      })
+      .exec();
       if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 
-      // TODO: If authenticated users => first => view log creation
+      if (memberId) {
+      //  Check Existence
+      const input: ViewInput = {
+            memberId: memberId,
+            viewRefId: productId,
+            viewGroup: ViewGroup.PRODUCT
+      }
+      const existView = await this.viewService.checkViewExistence(input);
+
+      console.log("exist:", !!existView);
+      if (!existView) {
+         // Insert View
+        await this.viewService.insertMemberView(input);
+
+          // Increase Counts
+          const result2 = await this.productModel
+          .findByIdAndUpdate(
+            productId, 
+            { $inc: { productViews: +1 } },
+            { new: true }
+          )
+          .exec();
+      }
+      }
 
       return result;
     }
