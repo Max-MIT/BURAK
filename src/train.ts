307032bab90d8bb0c7910e7e1e4f160d05ac6f7f
Max-MIT <maksudbek198501@gmail.com>
2025-03-11 05:04:34 +0900

    ZL-TASK:

--- src/train.ts
+++ src/train.ts
@@ -33,23 +33,40 @@ self destroy
     4) SCHEMA VALIDATION (DB)
 */
 //==========================================================================//
+// ZL-TASK:
+
+// Shunday function yozing, u parametrda berilgan stringni kebab casega otkazib qaytarsin. Bosh harflarni kichik harflarga ham otkazsin.
+// MASALAN: stringToKebab(“I love Kebab”) return “i-love-kebab”
+
+function stringToKebab(str: string): string {
+    return str
+        .toLowerCase() // Hammasini kichik harfga o'tkazish
+        .replace(/\s+/g, '-') // Bo'sh joylarni "-" bilan almashtirish
+        .replace(/[^a-z0-9\-]/g, ''); // Maxsus belgilarni olib tashlash
+}
+
+console.log(stringToKebab("I love Kebab")); // "i-love-kebab"
+console.log(stringToKebab("Hello World! 123")); // "hello-world-123"
+console.log(stringToKebab("TypeScript & JavaScript")); // "typescript-javascript"
+
+// =============================================================================
 // ZK-TASK:
 
 // Shunday function yozing, u har soniyada bir marta consolega 1 dan 5 gacha bolgan raqamlarni chop etsin va 5 soniyadan keyin ishini toxtatsin.
 // MASALAN: printNumbers()
 
-function printNumbers() {
-    let num = 1;
-    const interval = setInterval(() => {
-        console.log(num);
-        num++;
-        if (num > 5) {
-            clearInterval(interval);
-        }
-    }, 1000);
-}
+// function printNumbers() {
+//     let num = 1;
+//     const interval = setInterval(() => {
+//         console.log(num);
+//         num++;
+//         if (num > 5) {
+//             clearInterval(interval);
+//         }
+//     }, 1000);
+// }
 
-printNumbers();
+// printNumbers();
 
 
 // ============================================================================
